cf98141e61fffd4ca715dfd2f7efbba0845030a0
Atheer Alaghbari <atheeralaghbari61@gmail.com>
2019-10-17 20:55:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 // Code your solution in this file!
+<<<<<<< HEAD
 const companyName = 'Scuber';
 let     mostProfitableNeighborhood='Chelsea' ;
 let  companyCeo='Susan Smith';
+=======
+const companyName = 'Chelsea';
+>>>>>>> 33260c315af979a94922433f12d57c5cdff5681b
